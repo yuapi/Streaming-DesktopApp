5118a7482be9204c68f34328a61b4ffc865d477b
yuapi <phk2225@gmail.com>
2024-03-17 05:18:48 +0900

    Chzzk 아이콘 추가

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const { app, BrowserWindow } = require("electron");
+const path = require("path");
 
 const rpcController = require("./src/rpcController.js");
 
 const createWindow =() => {
 	const win = new BrowserWindow({
 		width: 1280,
-		height: 720
+		height: 720,
+		minWidth: 1280,
+		minHeight: 720,
+		icon: path.join(__dirname, "assets/icon/chzzk_ico.png")
 	})
 
+	win.setMenu(null);
 	win.loadURL("https://chzzk.naver.com");
 
 	rpcController.init(win);
